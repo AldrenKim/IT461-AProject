f5a680d515b5cbeff44ec24d56f834485c6dd44c
e6654321 <enespra@gmail.com>
2022-06-08 19:34:46 +0800

    added api

--- client/src/api/AnimalApi.ts
+++ client/src/api/AnimalApi.ts
@@ -4,8 +4,24 @@ import { API_VERSION, BASE_API_URL } from '../config';
 import { ApiRoute } from '../enums';
 import { BaseData, Animal } from '../types';
 
-const PLANTS_API_URL = `${BASE_API_URL}${API_VERSION}${ApiRoute.ANIMALS}?limit=3&offset=0`;
+const ANIMALS_API_URL = `${BASE_API_URL}${API_VERSION}${ApiRoute.ANIMALS}`;
 
 export async function getAnimals(axios: AxiosStatic, url?: string): Promise<BaseData<Animal>> {
-  return (await axios.get<BaseData<Animal>>(url || PLANTS_API_URL)).data;
+  return (await axios.get<BaseData<Animal>>(url || `${ANIMALS_API_URL}?limit=3&offset=0`)).data;
+}
+
+export async function getAnimal(axios: AxiosStatic, id: string): Promise<Animal> {
+  return (await axios.get<Animal>(`${ANIMALS_API_URL}/${id}`)).data;
+}
+
+export async function editAnimal(axios: AxiosStatic, data: Animal): Promise<Animal> {
+  return (await axios.put<Animal>(`${ANIMALS_API_URL}/${JSON.stringify(data)}`)).data;
+}
+
+export async function addAnimal(axios: AxiosStatic, data: Animal): Promise<Animal> {
+  return (await axios.post<Animal>(`${ANIMALS_API_URL}/${JSON.stringify(data)}`)).data;
+}
+
+export async function deleteAnimal(axios: AxiosStatic, id: string): Promise<Animal> {
+  return (await axios.delete<Animal>(`${ANIMALS_API_URL}/${id}`)).data;
 }
--- client/src/api/PlantApi.ts
+++ client/src/api/PlantApi.ts
@@ -4,8 +4,24 @@ import { API_VERSION, BASE_API_URL } from '../config';
 import { ApiRoute } from '../enums';
 import { BaseData, Plant } from '../types';
 
-const PLANTS_API_URL = `${BASE_API_URL}${API_VERSION}${ApiRoute.PLANTS}?limit=3&offset=0`;
+const PLANTS_API_URL = `${BASE_API_URL}${API_VERSION}${ApiRoute.PLANTS}`;
 
 export async function getPlants(axios: AxiosStatic, url?: string): Promise<BaseData<Plant>> {
-  return (await axios.get<BaseData<Plant>>(url || PLANTS_API_URL)).data;
+  return (await axios.get<BaseData<Plant>>(url || `${PLANTS_API_URL}?limit=3&offset=0`)).data;
+}
+
+export async function getPlant(axios: AxiosStatic, id: string): Promise<Plant> {
+  return (await axios.get<Plant>(`${PLANTS_API_URL}/${id}`)).data;
+}
+
+export async function editPlant(axios: AxiosStatic, data: Plant): Promise<Plant> {
+  return (await axios.put<Plant>(`${PLANTS_API_URL}/${JSON.stringify(data)}`)).data;
+}
+
+export async function addPlant(axios: AxiosStatic, data: Plant): Promise<Plant> {
+  return (await axios.post<Plant>(`${PLANTS_API_URL}/${JSON.stringify(data)}`)).data;
+}
+
+export async function deletePlant(axios: AxiosStatic, id: string): Promise<Plant> {
+  return (await axios.delete<Plant>(`${PLANTS_API_URL}/${id}`)).data;
 }
